cd00d6aa85d4cb391016c90a76622e83012f261b
lishu620 <mlishu@163.com>
2024-07-25 16:47:22 +0800

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,12 @@
 <template>
-    <div class="container">
-        <header class="left-column">
-            <h1>C区实验室</h1>
-            <p>欢迎各位来到C区实验室</p>
-        </header>
-        <main class="right-column">
-            <section class="feature">
-                <h2>我们是</h2>
-                <p>我们是重庆文理学院实训中心C区实验室</p>
-            </section>
-            <section class="feature">
-                <h2>我们的任务</h2>
-                <p>我们主要负责格致楼3楼C区机房的日常维护和教学管理工作</p>
-            </section>
-            <section class="feature">
-                <h2>我们的义务</h2>
-                <p>我们需要管理好机房的正常教学运行，保证师生正常进行教学任务</p>
-            </section>
-        </main>
-    </div>
+    <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      欢迎来到C区实验室
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">了解我们</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">加入我们</a>.
+    </h3>
+  </div>
 </template>
 
 <script>
@@ -28,41 +16,27 @@ export default {
 </script>
 
 <style scoped>
-/* 确保整个容器充满视窗 */
-.container {
-    display: flex;
-    flex-direction: row;
-    height: 100vh;
-    width: 100vw;
-    margin: 0;
-    padding: 0;
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
 }
 
-/* 左侧列样式 */
-.left-column {
-    flex: 0 1 auto;    /* 使其占据一半的宽度 */
-    display: flex;
-    height: auto;
-    align-items: center;    /*垂直对齐*/
-    justify-content: center;    /*水平对齐*/
-    background-color: #42b983;
-    color: white;
-    padding: 40px;
-    box-sizing: border-box;     /* 包含内边距在元素宽度内 */
+h3 {
+  font-size: 1.2rem;
 }
 
-/* 右侧列样式 */
-.right-column {
-    flex: 0 1 auto;
-    /* 使其占据一半的宽度 */
-    padding: 20px;
-    /* 合理设置内边距 */
-    box-sizing: border-box;
-    /* 包含内边距在元素宽度内 */
+.greetings h1,
+.greetings h3 {
+  text-align: center;
 }
 
-.feature {
-    margin-bottom: 40px;
-    /* 各个特性之间的间距 */
+@media (min-width: 1024px) {
+
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
 }
 </style>
