00fd82e56c83930bc73729187adb6ae456ba0c4d
lishu620 <mlishu@163.com>
2024-07-25 19:12:29 +0800

    21

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,42 +1,46 @@
 <template>
     <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      欢迎来到C区实验室
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">了解我们</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">加入我们</a>.
-    </h3>
-  </div>
+        <h1 class="green">{{ msg }}</h1>
+        <h3>
+            欢迎来到C区实验室
+            <a href="https://vitejs.dev/" target="_blank" rel="noopener">了解我们</a> +
+            <a href="https://vuejs.org/" target="_blank" rel="noopener">加入我们</a>.
+        </h3>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'Home'
+    name: 'Home',
+    props: {
+        msg: String
+    }
 }
 </script>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
+.greetings {
+    text-align: center;
 }
 
-h3 {
-  font-size: 1.2rem;
+.greetings h1 {
+    font-weight: 500;
+    font-size: 2.6rem;
+    position: relative;
+    top: -10px;
+    margin: 0;
+    /* 避免默认 margin 影响布局 */
 }
 
-.greetings h1,
 .greetings h3 {
-  text-align: center;
+    font-size: 1.2rem;
+    margin: 0;
+    /* 避免默认 margin 影响布局 */
 }
 
 @media (min-width: 1024px) {
-
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+    .greetings {
+        text-align: left;
+    }
 }
 </style>
